398b826940e402b35eb3fdda82e2edae52684aad
SarfarajSalimKhan <sarfaraz.salimkhan17@rediffmail.com>
2019-09-27 22:16:43 +0530

    Updated indentation error

--- Main.java
+++ Main.java
@@ -1,32 +1,32 @@
 class A
 {
-	void validAge ( int age )
+    void validAge ( int age )
+    {
+	if ( age < 18 )
 	{
-		if ( age < 18 )
-		{
-			throw new ArithmeticException ( "Age is not valid for admission." );
-		}
-		else
-		{
-			System.out.println ( "Age is valid for admission." );
-		}
-	} 
+	    throw new ArithmeticException ( "Age is not valid for admission." );
+	}
+	else
+	{
+	    System.out.println ( "Age is valid for admission." );
+    	}
+    } 
 }
 
 public class Main
 {
-	public static void main ( String args [ ] )
+    public static void main ( String args [ ] )
+    {
+	try
 	{
-		try
-		{
-			int num = 5 / 0;
-		}
-		catch ( ArithmeticException e )
-		{
-			System.out.println ( "Zero Division Error Exception." );
-			System.out.println ( "Exception is : " + e );
-		}
-		A obj = new A ( );
-		obj.validAge ( 17 );
+	    int num = 5 / 0;
 	}
+	catch ( ArithmeticException e )
+	{
+	    System.out.println ( "Zero Division Error Exception." );
+	    System.out.println ( "Exception is : " + e );
+	}
+	A obj = new A ( );
+	obj.validAge ( 17 );
+    }
 }
